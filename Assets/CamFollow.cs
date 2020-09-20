d04df5dfe2f2fc0d8dadcc8ddb407c860a64c434
Peter <bgs303@nyu.edu>
2020-09-20 14:40:49 -0400

    Initial commit

---
+++ Assets/CamFollow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class CamFollow : MonoBehaviour
+{
+    public GameObject player;
+
+    private Vector3 offset;
+
+   
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
---
+++ Assets/Trigger12.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger12 : MonoBehaviour
+{
+    public Camera camera1;
+    public Camera camera2;
+    public Camera camera3;
+    public Camera camera4;
+    public Camera camera5;
+    public Camera camera6;
+    public Camera camera7;
+    public Camera camera8;
+    public Camera camera9;
+    public Camera camera10;
+    public Camera camera11;
+    public Camera camera12;
+    public Camera camera13;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        camera1.enabled = false;
+        camera2.enabled = false;
+        camera3.enabled = false;
+        camera4.enabled = false;
+        camera5.enabled = false;
+        camera6.enabled = false;
+        camera7.enabled = false;
+        camera8.enabled = false;
+        camera9.enabled = false;
+        camera10.enabled = false;
+        camera11.enabled = false;
+        camera12.enabled = false;
+        camera13.enabled = true;
+    }
+    }
